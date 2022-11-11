75b8213e98a32d5656c3133b02fab2228cea5edc
Arina Kohanova <arinakokhanova@gmail.com>
2022-11-11 13:47:11 +0300

    remodelled

--- PersonalQuiz/ViewControllers/ViewController.swift
+++ PersonalQuiz/ViewControllers/ViewController.swift
@@ -8,10 +8,12 @@
 import UIKit
 
 class ViewController: UIViewController {
+    
+    private let questions = Question.getQuestion()
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        
     }
 
 
